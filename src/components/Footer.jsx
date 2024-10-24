ab3872d55f4baa517f3b0238aa06ca1376de9f12
sahal2255 <sahalvv9656@gmail.com>
2024-10-24 17:34:37 +0530

    update the

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import { FaGithubSquare, FaInstagram } from 'react-icons/fa'
+import React from 'react';
+import { FaGithubSquare, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <div className='max-w-[1300px] mx-auto flex justify-between p-6 md:p-20 text-sm md:text-lg mt-12'>
-        <div className='space-y-4'>
-            <h3 className='text-2xl text-gray-200 font-semibold'>SAHAL V V</h3>
-            <div className='flex flex-row gap-6 text-gray-400 text-4xl'>
-            <a href="https://github.com/sahal2255" target="_blank" rel="noopener noreferrer">
-              <FaGithubSquare />
-            </a>
-                <a href="https://instagram.com/_.sahal_vv"><FaInstagram /></a>
-            </div>
-
+      <div className='space-y-4'>
+        <h3 className='text-2xl text-gray-200 font-semibold'>SAHAL V V</h3>
+        <div className='flex flex-row gap-6 text-gray-400 text-4xl'>
+          <a href="https://github.com/sahal2255" target="_blank" rel="noopener noreferrer">
+            <FaGithubSquare />
+          </a>
+          <a href="https://instagram.com/_.sahal_vv" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
         </div>
+      </div>
 
-        <p className='text-gray-400'>sahalvv9656@gmail.com</p>
-        
+      <div className='text-gray-400'>
+        <p>sahalvv9656@gmail.com</p>
+        <p>(+91) 9645228096</p> {/* Replace with your actual phone number */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
