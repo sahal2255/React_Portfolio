9d3a2586fc6c26f6cb5d3cc9bc19fda26cb8af3f
sahal2255 <sahalvv9656@gmail.com>
2024-12-05 10:17:26 +0530

    main project updated

--- src/components/Mainpro.jsx
+++ src/components/Mainpro.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import project6 from "../assets/dryvitals.png";
-import { AiFillGithub, AiOutlineGithub } from 'react-icons/ai';
+import QuickFix from '../assets/Quick_fiz.png';
+import { AiFillGithub } from 'react-icons/ai';
 import Reveal from './Reveal';
 
 const projects = [
   {
     img: project6,
-    title: "DryVitals.shop",
+    title: "DryVitals",
     description: `DryVitals.shop is an e-commerce website built to provide users with a seamless shopping experience. The frontend is developed using HTML, CSS, and Bootstrap, ensuring a responsive and visually appealing interface. On the server side, Node.js using to create a backend while MongoDB serves as the database for managing product listings, user data, and orders.`,
     links: {
       site: "https://dryvitals.shop",
       github: "https://github.com/sahal2255/dryvitals",
+    },
+  },
+  {
+    img: QuickFix,
+    title: "QuickFix",
+    description:
+      "Quick Fix is an innovative web platform designed to connect users with professional service providers for their home and business needs. The platform streamlines the process of booking skilled professionals for tasks such as repairs, maintenance, and other essential services. Built using the MERN stack (MongoDB, Express.js, React.js, and Node.js).",
+    links: {
+      site: "https://quickfix-frontend.onrender.com/",
+      githubFrontend: "https://github.com/sahal2255/QuickFix_Backend",
+      githubBackend: "https://github.com/sahal2255/QuickFix_Frontend",
     },
   },
 ];
@@ -21,8 +33,10 @@ const Portfolio = () => {
       <h2 className='text-3xl font-bold text-gray-200 mb-8'>Main Projects</h2>
       {projects.map((project, index) => (
         <Reveal key={index}>
-          <div 
-            className={`flex flex-col md:flex-row ${index % 2 !== 0 ? 'md:flex-row-reverse' : ''} mb-12`}
+          <div
+            className={`flex flex-col md:flex-row ${
+              index % 2 !== 0 ? 'md:flex-row-reverse' : ''
+            } mb-12`}
           >
             <div className='w-full md:w-1/2 p-4 flex justify-center'>
               <img
@@ -32,17 +46,44 @@ const Portfolio = () => {
               />
             </div>
             <div className='w-full md:w-1/2 p-4 flex flex-col justify-center'>
-              <h3 className='text-2xl font-semibold text-gray-200 mb-4'>{project.title}</h3>
+              <h3 className='text-2xl font-semibold text-gray-200 mb-4'>
+                {project.title}
+              </h3>
               <p className='text-gray-300 mb-4'>{project.description}</p>
               <div className='flex space-x-4'>
-                <a href={project.links.site}
-                   className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700 transition duration-300'>
+                <a
+                  href={project.links.site}
+                  className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700 transition duration-300'
+                >
                   View Site
                 </a>
-                <a href={project.links.github}
-                   className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700 transition duration-300'>
-                  <AiOutlineGithub/>
-                </a>
+                {project.links.github && (
+                  <a
+                    href={project.links.github}
+                    className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700 transition duration-300 flex items-center space-x-2'
+                  >
+                    <AiFillGithub />
+                    <span>GitHub</span>
+                  </a>
+                )}
+                {project.links.githubFrontend && (
+                  <a
+                    href={project.links.githubFrontend}
+                    className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700 transition duration-300 flex items-center space-x-2'
+                  >
+                    <AiFillGithub />
+                    <span>Frontend</span>
+                  </a>
+                )}
+                {project.links.githubBackend && (
+                  <a
+                    href={project.links.githubBackend}
+                    className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700 transition duration-300 flex items-center space-x-2'
+                  >
+                    <AiFillGithub />
+                    <span>Backend</span>
+                  </a>
+                )}
               </div>
             </div>
           </div>
